a0e6b58f39b57507019f8661bbfc3a8341bb7e55
at-daihv <daihv@aisantech.vn>
2016-02-16 13:44:30 +0700

    1

--- TravisCI/AppDelegate.swift
+++ TravisCI/AppDelegate.swift
@@ -45,7 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "vn.at.TravisCI" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
     }()
