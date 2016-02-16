4129bb2c60ee21ef08bad064fc63b9425348105b
DaiHV <daihv@asiantech.vn>
2016-02-16 18:03:06 +0700

    a

--- .travis.before_install.bash
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  echo "linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux linux"
-elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  brew update
-  brew install swiftlint
-	echo "swiftlint swiftlint swiftlint swiftlint swiftlint swiftlint swiftlint swiftlint swiftlint"
-fi
